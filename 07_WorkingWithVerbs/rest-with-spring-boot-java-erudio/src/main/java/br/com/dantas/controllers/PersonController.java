a240bb566c76956e13a04bef0be0b0fe035ba11c
Daniel Dantas <dantas.dan.ddr@gmail.com>
2023-11-10 17:34:50 -0300

    Working With Verbs

---
+++ 07_WorkingWithVerbs/rest-with-spring-boot-java-erudio/src/main/java/br/com/dantas/controllers/PersonController.java
@@ -0,0 +1,56 @@
+package br.com.dantas.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.dantas.model.Person;
+import br.com.dantas.services.PersonServices;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+	
+	//private PersonServices services = new PersonServices();
+	@Autowired
+	private PersonServices services;
+	
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll() {
+		return services.findAll();
+	}
+	
+	@GetMapping(value = "/{id}",
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person findById(@PathVariable(value = "id") String id){
+		return services.findById(id);
+	}
+	
+	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person){
+		return services.create(person);
+	}
+	
+	@PutMapping(produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person){
+		return services.update(person);
+	}
+	
+	@DeleteMapping(value = "/{id}",
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public void delete(@PathVariable(value = "id") String id){
+		services.delete(id);
+	}
+
+}
