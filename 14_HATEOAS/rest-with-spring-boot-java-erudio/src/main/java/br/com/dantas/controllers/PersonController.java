997df5958017951cae26e08173f85805073dd426
Daniel Dantas <dantas.dan.ddr@gmail.com>
2023-11-26 19:55:29 -0300

    HATEOAS, Testing PersonServices with mokito and junit5

---
+++ 14_HATEOAS/rest-with-spring-boot-java-erudio/src/main/java/br/com/dantas/controllers/PersonController.java
@@ -0,0 +1,60 @@
+package br.com.dantas.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.dantas.data.vo.v1.PersonVO;
+import br.com.dantas.services.PersonServices;
+import br.com.dantas.util.MediaType;
+
+@RestController
+@RequestMapping("/api/person/v1")
+public class PersonController {
+	
+	//private PersonServices services = new PersonServices();
+	@Autowired
+	private PersonServices services;
+	
+	@GetMapping(produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
+	public List<PersonVO> findAll() {
+		return services.findAll();
+	}
+	
+	@GetMapping(
+			value = "/{id}",
+			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
+	public PersonVO findById(@PathVariable(value = "id") Long id){
+		return services.findById(id);
+	}
+	
+	@PostMapping(
+			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML },
+			consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
+	public PersonVO create(@RequestBody PersonVO person){
+		return services.create(person);
+	}
+	
+	@PutMapping(
+			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML },
+			consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
+	public PersonVO update(@RequestBody PersonVO person){
+		return services.update(person);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id){
+		services.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+}
