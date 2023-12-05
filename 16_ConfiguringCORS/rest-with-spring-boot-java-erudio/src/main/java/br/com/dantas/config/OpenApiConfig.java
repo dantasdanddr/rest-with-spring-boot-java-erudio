ee2c7c4705d043346e057ba87923b4677a6bcdbe
dantasdanddr <dantas.dan.ddr@gmail.com>
2023-12-05 20:58:17 -0300

    Configuring CORS

---
+++ 16_ConfiguringCORS/rest-with-spring-boot-java-erudio/src/main/java/br/com/dantas/config/OpenApiConfig.java
@@ -0,0 +1,29 @@
+package br.com.dantas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    OpenAPI customOpenAPI() {
+		return new OpenAPI()
+			.info(new Info()
+				.title("RESTFull API with Java 20 and Spring Boot 3.1.6")
+				.version("v1")
+				.description("Some description about your API")
+				.termsOfService("https://github.com/dantasdanddr/rest-with-spring-boot-java-erudio")
+				.license(
+					new License()
+						.name("Apache 2.0")
+						.url("https://github.com/dantasdanddr/rest-with-spring-boot-java-erudio")
+					)
+				);
+	}
+
+}
